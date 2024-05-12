0c41ce56fa6541d0d672eff488af8961b8febcbc
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-12 14:13:48 +0300

    Add mouse click and mouse down draw functionality

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const container = document.querySelector(".container");
+
 const GRID_SIZE = 16;
+const currentColor = "black";
+
+let isMouseDown = false;
 
 function createPixelGrid(size) {
   for (let i = 0; i < size; ++i) {
@@ -17,4 +21,29 @@ function createElementWithClass(elementName, className) {
   return element;
 }
 
+function changePixelColor(pixel) {
+  pixel.style.backgroundColor = currentColor;
+}
+
 createPixelGrid(GRID_SIZE);
+
+const rows = document.querySelectorAll(".row");
+
+rows.forEach((row) => {
+  row.addEventListener("mousemove", (e) => {
+    if (isMouseDown) {
+      changePixelColor(e.target);
+    }
+  });
+  row.addEventListener("mousedown", (e) => {
+    changePixelColor(e.target);
+  });
+});
+
+addEventListener("mousedown", () => (isMouseDown = true));
+addEventListener("mouseup", () => (isMouseDown = false));
+
+// there was a bug, when we tried to drag some pixel
+// because of that mouseup event didn't work and
+// the cursor continued to draw when the mouse key was up
+addEventListener("dragstart", (e) => e.preventDefault());
