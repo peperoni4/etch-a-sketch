f8a42d6224ee73fb4ddcd9c0244e193256ff79c7
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-12 12:39:42 +0300

    Add functionality to create pixel grid in HTML
    Function createPixelGrid has size parameter for future feature, so that
    the users can resize the grid size manually if they want to. For now, it
    is just a hard-coded value in a variable.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("test message");
+const container = document.querySelector(".container");
+const GRID_SIZE = 16;
+
+function createPixelGrid(size) {
+  for (let i = 0; i < size; ++i) {
+    const row = createElementWithClass("div", "row");
+    for (let j = 0; j < size; ++j) {
+      row.appendChild(createElementWithClass("div", "pixel"));
+    }
+    container.appendChild(row);
+  }
+}
+
+function createElementWithClass(elementName, className) {
+  const element = document.createElement(elementName);
+  element.classList.add(className);
+  return element;
+}
+
+createPixelGrid(GRID_SIZE);
